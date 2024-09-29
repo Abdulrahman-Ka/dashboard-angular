9ebda39e7f501f71d1d7594c959134d56223cee3
abode.kalls <abode.kalls@gmail.com>
2024-09-29 20:31:26 +0000

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousigLocationComponent} from '../housig-location/housig-location.component'
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousigLocationComponent],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location 
+        *ngFor="let housingLocation of housingLocationList"
+        [housingLocation] = "housingLocation"
+       ></app-housing-location>
+</section>
+  `,
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+
+  housingLocationList: HousingLocation[] =[];
+  
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+/*
+ *  Protractor support is deprecated in Angular.
+ *  Protractor is used in this example for compatibility with Angular documentation tools.
+ */
+import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
+
+import { provideRouter } from '@angular/router';
+import routerConfig from './app/routes';
+
+bootstrapApplication(AppComponent, {providers: [provideProtractorTestingSupport(), provideRouter(routerConfig)]}).catch((err) =>
+  console.error(err),
+);
